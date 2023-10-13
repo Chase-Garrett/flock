9cb0ee07d924303060b1e050aeb8c5ba26987f9c
Chase <chasegarrett@tutanota.com>
2023-10-13 16:00:22 -0500

    add controllers for models

---
+++ controller/postController.js
@@ -0,0 +1,33 @@
+const { User, Post } = require("../../models");
+
+module.exports = {
+  // get all posts
+  async getPosts(req, res) {
+    try {
+      const posts = await Post.find();
+      res.json(posts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // create a new post
+  async createPost(req, res) {
+    try {
+      const post = await Post.create(req.body);
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $push: { posts: post._id } },
+        { new: true, runValidators: true }
+      );
+
+      if (!updatedUser) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+
+      res.json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+};
